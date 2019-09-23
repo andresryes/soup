735fdf921dba89b968230107b15dc14b33212cfd
andresryes <enriquebolanos@ufm.edu>
2019-09-22 21:51:14 -0600

    finished estudios

--- soup/estudios.py
+++ soup/estudios.py
@@ -17,15 +17,32 @@ class Estudios:
         result.append("---------------------------------------")
 
         # display ALL "Estudios" (Doctorados/Maestrias/Posgrados/Licenciaturas/Baccalaureus)
-        result.append(f"display ALL \"Estudios\" (Doctorados/Maestrias/Posgrados/Licenciaturas/Baccalaureus):")
-        resEst = []
-        for i in soup.find_all(class_="estudios")[0].parent.find_all('p'):
-            resEst.append(i.text)
-            
-        for i in soup.find_all(class_="estudios")[0].parent.parent.parent.parent.find_all('div')[2]:
-            print(i)
-        print(resEst)
+        result.append(f"display ALL \"Estudios\" (Doctorados/Maestrias/Posgrados/Licenciaturas/Baccalaureus):  Output exceeds 30 lines, sending output to: logs/2estudios_display_all_estudios.txt")            
+
+        f = open("../logs/2estudios_display_all_estudios.txt","w+")
+        if(len(soup.find_all(class_="estudios")[0].parent.parent.parent.parent.find_all('div')[0].find_all('a')) > 30):
+            for i in soup.find_all(class_="estudios")[0].parent.parent.parent.parent.find_all('div')[0].find_all('a'):
+                if i.text != "":
+                    f.writelines("-"+i.text+"\r\n")
+        else:
+            result.append("-"+i.text)
         result.append("---------------------------------------")
 
+        #display from "leftbar" all <li> items (4 in total)
+        result.append(f"display from \"leftbar\" all <li> items (4 in total)")            
+        for i in soup.find_all(class_="leftbar")[0].find_all('li'):
+            result.append("-"+i.find('a').text)
+        result.append("---------------------------------------")
+        # get and display all available social media with its links (href) "class=social pull-right"
+        result.append(f"get and display all available social media with its links (href) \"class=social pull-right\"")
+        
+        for i in soup.find_all(class_="social pull-right")[0].find_all('a'):
+            result.append("-"+i['href'])
+
+        result.append("---------------------------------------")
+
+        # count all <a> (just display the count)
+        result.append(f"count all <a> (just display the count): {len(soup.find_all('a'))}")
+        result.append("---------------------------------------")
 
         return result
